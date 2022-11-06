b885f31793be6a55b88d33c32c7916403ef96cd0
Avichal Shukla <shuavi710500@gmail.com>
2022-11-06 17:23:12 +0530

    Final commit

---
+++ app/src/main/java/com/example/aroundtheworld/EntertainmentFragment.java
@@ -0,0 +1,62 @@
+package com.example.aroundtheworld;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EntertainmentFragment extends Fragment {
+
+    String api = "622ff393c45f48d28abb721f3ffc6362";
+    ArrayList<ModelClass> modelClassArrayList;
+    Adapter adapter;
+    String country = "in";
+    private RecyclerView recyclerViewOfEnter;
+    private String category = "entertainment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.entertainmentfragment, null);
+
+        recyclerViewOfEnter = v.findViewById(R.id.entertainmentrv);
+        modelClassArrayList = new ArrayList<>();
+        recyclerViewOfEnter.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new Adapter(getContext(), modelClassArrayList);
+        recyclerViewOfEnter.setAdapter(adapter);
+
+        findNews();
+
+        return v;
+    }
+
+    private void findNews() {
+
+        ApiUtilities.getApiInterface().getCategoryNews(country, category, 100, api).enqueue(new Callback<mainNews>() {
+            @Override
+            public void onResponse(Call<mainNews> call, Response<mainNews> response) {
+                if (response.isSuccessful()) {
+                    modelClassArrayList.addAll(response.body().getArticles());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<mainNews> call, Throwable t) {
+
+            }
+        });
+    }
+}
